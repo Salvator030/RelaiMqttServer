bbc13c58108020850e822c02130f001224860b20
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-06-07 03:24:02 +0200

    iplement primevue

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,5 +1,18 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import '@/assets/styles.scss';
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import PrimeVue from 'primevue/config';
+const app = createApp(App)
+
+app.use(PrimeVue, {ripple: true});
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,18 +1,24 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<script setup>
+import Card from 'primevue/card';
+import Button from 'primevue/button';
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+const serverStat = ['stop','load',''];
+const options = ref(['Stop', 'Start']);
+
 </script>
+
+<template>
+  <Card style="width: 15rem;"><template #title>
+      <h1>Server</h1>
+    </template>
+    <template #content>
+      <div class="flex justify-content-center gap-2">
+        <Button label="Stop Server" />
+        <Button label="Start Server" />
+
+      </div>
+    </template>
+  </Card>
+</template>
