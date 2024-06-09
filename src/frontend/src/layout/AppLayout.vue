8471e442c2e4cb407c260442c111dc5f14fedfe0
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-06-09 03:11:40 +0200

    -customize AppLayout

--- src/frontend/src/layout/AppLayout.vue
+++ src/frontend/src/layout/AppLayout.vue
@@ -66,12 +66,12 @@ const isOutsideClicked = (event) => {
             <div class="layout-main">
                 <router-view></router-view>
             </div>
-            <app-footer></app-footer>
+     <!--    <app-footer></app-footer> -->   
         </div>
-        <app-config></app-config>
+        <!--  <app-config></app-config> -->   
         <div class="layout-mask"></div>
     </div>
-    <Toast />
+   <!-- <Toast /> -->
 </template>
 
 <style lang="scss" scoped></style>
