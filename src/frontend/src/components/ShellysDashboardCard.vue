7cd1e1c45c4f58b795bf9dd4170a1104e3d5d8bd
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2024-06-09 13:35:40 +0200

    wip implement card for shellys

---
+++ src/frontend/src/components/ShellysDashboardCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from 'primevue/card';
+
+const props = defineProps({shellysList: []});
+</script>
+
+<template>
+    <Card>
+        <template #title>
+        <h1>Shellys</h1>
+      </template>
+      <template #content>
+        <div v-for="(item) in props.shellysList" :key="item.id">
+            <p>abc</p>
+        </div>
+      </template>
+    </Card>
+ </template>
